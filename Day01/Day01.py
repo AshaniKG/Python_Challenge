17d6f92e4fdf392e959320cbe24cc7e5f7d928cd
KGAMadushika <ashanim683@gmail.com>
2024-12-09 21:35:19 +0530

    Add files via upload
    First day of my python challenge

---
+++ Day01/Day01.py
@@ -0,0 +1,9 @@
+print ("HI THERE..!\n")
+
+Name = input ("Enter your name: ")
+Age = input ("Enter your age: ")
+FColour = input ("Enter your favourite colour: ")
+
+print ("\nName: " + Name)
+print ("Age :" + Age)
+print ("Favourite Colour: " + FColour)
